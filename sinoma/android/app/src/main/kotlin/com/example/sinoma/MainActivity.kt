f205068845c1dd9a2687a8474bbc188da8b3af70
fathansantoso30 <fathansantoso30@gmail.com>
2021-05-19 09:42:10 +0700

    First commit

---
+++ sinoma/android/app/src/main/kotlin/com/example/sinoma/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.sinoma
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ sinoma/lib/theme.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+Color darkblueColor = Color(0xff2F2E41);
+Color blueColor = Color(0xff1FA2FF);
+Color greyColor = Color(0xff7D7D7D);
+Color lightgreyColor = Color(0xffD3D3D3);
+Color blackColor = Color(0xff3F414E);
+Color whiteColor = Color(0xffFFFFFF);
+
+TextStyle regulargreyText = GoogleFonts.roboto(
+  fontWeight: FontWeight.normal,
+  color: greyColor,
+);
+
+TextStyle boldwhiteText = GoogleFonts.roboto(
+  fontWeight: FontWeight.bold,
+  color: whiteColor,
+);
+
+TextStyle boldblueText = GoogleFonts.roboto(
+  fontWeight: FontWeight.bold,
+  color: blueColor,
+);
+
+TextStyle boldblackText = GoogleFonts.roboto(
+  fontWeight: FontWeight.bold,
+  color: blackColor,
+);
